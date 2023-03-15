2a80d9b27bb5370a5747f049d36067276142c61d
Eugene <asd@gmail.com>
2023-03-16 01:36:05 +0200

    [add] data fetching // bad

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,12 +1,20 @@
+import { FC } from "react";
 import { DataBlock } from "@/components/DataBlock";
 import { MoneyBlock } from "@/components/MoneyBlock";
 import { Wrapper } from "@/components/Wrapper";
 import styles from "@/styles/Dashboard.module.css";
 
-const Dashboard = () => {
+interface DashBoardProps {
+  data?: any;
+}
+
+const Dashboard: FC<DashBoardProps> = ({ data }) => {
+  console.log("🚀 ~ file: dashboard.tsx:12 ~ data:", data)
+  
   return (
     <Wrapper>
       <h1 className="pageTitle thin">dashboard</h1>
+      <pre>{data}</pre>
       <div className={styles.line}>
         <MoneyBlock isIncome={true} amount={200} percent={0} />
         <MoneyBlock isIncome={false} amount={100} percent={0} />
@@ -20,3 +28,17 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
+
+export async function getServerSideProps(context:any) {
+  console.log(context)
+  try {
+    const response = await fetch(
+      "https://c8ba3234-0cd5-4176-8def-44f7b79b2f8c.mock.pstmn.io/transactions"
+    );
+    const data = response.json();
+    console.log("🚀 ~ file: dashboard.tsx:38 ~ getServerSideProps ~ response:", data )
+    return { props: { data } };
+  } catch (error) {
+    return error;
+  }
+}
